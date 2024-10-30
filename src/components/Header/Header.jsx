767be081b005af0aeedb1dea2e95102c6d641fe3
knightoffcode <isaiaspereira103@yahoo.com>
2024-10-30 10:11:47 -0300

    #013 - Refactoring.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,18 +30,6 @@ function Header() {
                 </div>
                 <nav className='header__menu'>
                     <ul className='header__menu__list'>
-                        <div className='header__menu__item'>
-                            <div className='header__submenu'>
-                                <ul className='header__submenu__list'>
-                                    <li className='header__submenu__item'>
-                                        <a
-                                            className='header__submenu__anchor'
-                                            href='{subpage.url}'
-                                        ></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
                         {menuPages.map((page, index) => (
                             <div key={index} className='header__menu__item'>
                                 <li className='header__item'>
